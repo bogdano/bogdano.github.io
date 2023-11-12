b8d8a8b536b711349252129b83b01605e76e4940
bogdan <bogdan.bokovic@umontana.edu>
2023-11-12 12:30:21 -0700

    remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
 	// Enable React to support React JSX components.
-	integrations: [react()],
+	// integrations: [react()],
 	site: 'https://bogz.dev',
 });
